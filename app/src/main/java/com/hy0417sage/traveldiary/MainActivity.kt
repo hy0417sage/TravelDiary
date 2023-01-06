b5c59876f1757127c936dcc4ba30f4cb0e2b4a38
Song HyunYoung <97173983+hy0417sage@users.noreply.github.com>
2023-01-07 05:12:00 +0900

    checkLocationPermission 추가
    - 현재위치 수집을 위해 위치 퍼미션 추가

--- app/src/main/java/com/hy0417sage/traveldiary/MainActivity.kt
+++ app/src/main/java/com/hy0417sage/traveldiary/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.hy0417sage.traveldiary
 
+import android.Manifest
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -15,10 +17,14 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setBottomNavigationView()
+        checkLocationPermission()
+    }
+
+    private fun setBottomNavigationView(){
         val navView: BottomNavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         val appBarConfiguration = AppBarConfiguration(
@@ -26,8 +32,27 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_map
             )
         )
-
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    private fun checkLocationPermission(){
+        val locationPermissionRequest = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            when {
+                permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                    // Precise location access granted.
+                }
+                permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                    // Only approximate location access granted.
+                } else -> {
+                // No location access granted.
+            }
+            }
+        }
+        locationPermissionRequest.launch(arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION))
     }
 }
